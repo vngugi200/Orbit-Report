92e6ad1603f100910af8e623ed6e67568931bf9d
Viola Ngugi <Viola_Ngugi@cable.comcast.com>
2021-06-18 18:10:54 -0400

    assignment 6 submission

---
+++ src/app/satellite.ts
@@ -0,0 +1,30 @@
+export class Satellite {
+    name: string;
+    orbitType: string;
+    type: string;
+    operational: boolean;
+    launchDate: string;
+
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+    this.name = name;
+    this.type = type;
+    this.launchDate = launchDate;
+    this.orbitType = orbitType;
+    this.operational = operational;
+    }
+    shouldShowWarning (): boolean {
+        if (this.type.toLowerCase() === 'space debris'){
+        return true;
+        }
+        return false;
+      }
+}
+
+
+
+
+
+
+
+
+
